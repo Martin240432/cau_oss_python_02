d192c55ee9ea00c27a7785117a56898f0206eea9
Martin240432 <163376789+Martin240432@users.noreply.github.com>
2024-03-14 13:16:17 +0900

    Create prac1.py
    1주차 부피구하는 과

---
+++ prac1.py
@@ -0,0 +1,7 @@
+length = float(input("length : "))
+width = float(input("width : "))
+height = float(input("input : "))
+
+volume = length * width * height
+
+print(" The volume of box is ", volume)
